94c9f46d5d1f1076ee989206fe69142973dc191d
Hammad Khan <hammad283khan@gmail.com>
2024-03-29 12:55:09 +0500

    Feat:Minor Bug fixes

--- repeat/src/components/RepeatWorks.jsx
+++ repeat/src/components/RepeatWorks.jsx
@@ -12,8 +12,8 @@ const RepeatWorks = () => {
 
         <div className='flex justify-between flex-col sm:flex-row'>
             <div className='w-[95%] sm:[50%] m-2'>
-            <h2 className='font-semibold text-left text-[20px] mb-3 ml-3 md:mb-6 leading-[1.5]'>INGEST AND ANALYZE</h2>
-            <p className='m-3 leading-[1.5]  text-sm  md:text-left'>
+            <h2 className='font-semibold text-left text-[20px] lg:text-2x1 mb-3 ml-3 md:mb-6 leading-[1.5]'>INGEST AND ANALYZE</h2>
+            <p className='m-3 leading-[1.5]  text-sm  md:text-left md:text-lg lg:text-xl'>
             Repeat connects to your Shopify store to ingest all of your historical orders and each new order that your customers place. We analyze those orders at the store level to identify trends and at the customer level to understand individual behavior.            </p>
             </div>
             <div className='w-[80%] sm:[50%] pt-10 mx-auto'>
@@ -23,8 +23,8 @@ const RepeatWorks = () => {
 
         <div className='flex justify-between flex-col sm:flex-row'>
             <div className='w-[95%] sm:[50%] m-2'>
-            <h2 className='font-semibold text-left text-[20px] mb-3 ml-3  md:mb-6 leading-[1.5]'>TRANSFORM AND REPEAT</h2>
-            <p className='m-3 leading-[1.5]  text-sm  md:text-left'>
+            <h2 className='font-semibold text-left text-[20px] mb-3 ml-3 lg:text-2x1 md:mb-6 leading-[1.5]'>TRANSFORM AND REPEAT</h2>
+            <p className='m-3 leading-[1.5]  text-sm  md:text-left md:text-lg lg:text-xl'>
             Repeat feels like magic, but we're not a black box. We utilize your store's data to produce valuable insights and reports that help you level up your retention strategy with the same intelligence that fuels Repeat's tools.
             </p>
             </div>
@@ -35,8 +35,8 @@ const RepeatWorks = () => {
 
         <div className='flex justify-between flex-col sm:flex-row'>
             <div className='w-[95%] sm:[50%] m-5'>
-            <h2 className='font-semibold text-left text-[20px] mb-3 ml-3 md:mb-6 leading-[1.5]'>AUTOMATE AND ACT</h2>
-            <p className='m-3 leading-[1.5]  text-sm  md:text-left'>
+            <h2 className='font-semibold text-left text-[20px] mb-3 ml-3 md:mb-6 lg:text-2x1 leading-[1.5]'>AUTOMATE AND ACT</h2>
+            <p className='m-3 leading-[1.5]  text-sm md:text-lg lg:text-xl md:text-left'>
             Repeat generates events that you can use to automate flows and enriches customer profiles with data for personalization and segmentation. It all flows seamlessly into Klaviyo, Postscript, and Attentive to give you new ways to optimize your retention workflow.
             </p>
             </div>
